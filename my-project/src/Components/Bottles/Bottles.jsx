d00a4bb453ec9db82f03a251d1ccf7b24a6374bb
Md <dipum0505@gmail.com>
2025-03-28 17:27:23 +0600

    show all data and make it grid layout

--- my-project/src/Components/Bottles/Bottles.jsx
+++ my-project/src/Components/Bottles/Bottles.jsx
@@ -1,14 +1,19 @@
 import React, { use } from 'react';
 import Bottle from './bottle/bottle';
+import './Bottles.css'
 const Bottles = ({bottlesPromise}) => {
     const bottles = use(bottlesPromise)
      console.log(bottles)
     return (
         <div>
             <h3>Bottles : {Bottles.length}</h3>
-            {
+          
+         <div className='bottles-container'>
+           {
                 bottles.map(bottle => <Bottle bottle={bottle}></Bottle>)
             }
+         </div>
+          
         </div>
     );
 };
--- my-project/src/Components/Bottles/bottle/Bottle.jsx
+++ my-project/src/Components/Bottles/bottle/Bottle.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import './Bottle.css'
 
 const Bottle = ({bottle}) => {
-    console.log(bottle)
+     console.log(bottle)
+    const {img , name , price , stock} = bottle
     return (
-        <div>
-            <img src={bottle.img} alt="" />
+        <div className='card bottle'>
+            <img src={img} alt="" />
+            <h3>{name}</h3>
+            <p>${price}</p>
+            <p>Available: ${stock} remaining</p>
+            <button>Buy Now</button>
         </div>
     );
 };
